4eab4bd27fddcf15590d5007516ae94dc75daa84
Will DiFruscio <wdifruscio@gmail.com>
2017-01-19 21:42:16 -0500

    array to list

--- array-list.js
+++ array-list.js
@@ -1,18 +1,14 @@
 //write a function that takes an array, and turns it into a list
 //write a function that takes a list and turns it into an array.
 
+
 function arrayToList(arr) {
     list = null;
-    for (var i = 0; i< arr.length; i++) {
-        list = {value:arr[i], rest: list}
+
+    for (var i = 0; i < arr.length; i++){
+        list = {value: arr[i], rest:list }
     }
-    return console.log(list);
+    return list;
 }
-
-function listToArray(list) {
-    arr = 0;
-    
-}
-
-listToArray({ value: 3, rest: { value: 2, rest: { value: 1, rest: null } } });
-arrayToList([1,2,3]);
+var myArr = [5,6,7,8,9]
+console.log(arrayToList(myArr));
